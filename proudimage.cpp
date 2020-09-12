38bfa1e4b810b0117ad4daabf0035fd0252f3f01
mmjvox <mmdjv.kh94@gmail.com>
2020-09-13 03:48:45 +0430

    add files 0

---
+++ proudimage.cpp
@@ -0,0 +1,6 @@
+#include "proudimage.h"
+
+ProudImage::ProudImage()
+{
+
+}
---
+++ proudimage.h
@@ -0,0 +1,16 @@
+#ifndef PROUDIMAGE_H
+#define PROUDIMAGE_H
+
+#include <QQuickItem>
+
+class ProudImage : public QQuickItem
+{
+    Q_OBJECT
+public:
+    ProudImage();
+
+signals:
+
+};
+
+#endif // PROUDIMAGE_H
